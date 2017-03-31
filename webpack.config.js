64848e4ae6ab0ba40b2c4a10c786e226607fddcc
Krishang <Krishang>
2017-03-31 22:08:55 +0530

    #1 - setup initial project setups

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack.bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'},
+      {test: /\.css$/, use: 'css-loader'}
+    ]
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'src')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, 'src/index.html'),
+    })
+  ]
+};
